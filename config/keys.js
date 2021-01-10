8ec08671a4a64790a22ee8d08b3d540bbf17a34f
AnAtoliyAK <aliaksandrau89@gmail.com>
2021-01-10 22:40:00 +0300

    fix: add pasport to be

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
- const config = require('./config.env')
-
-const mongoURI = `mongodb+srv://${config.login}:${config.password}@cluster0.b9kug.mongodb.net/<dbname>?retryWrites=true&w=majority`
-const jwt = 'dev-jwt'
-
-exports.mongoURI = mongoURI
-exports.jwt = jwt
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod')
+} else {
+  module.exports = require('./keys.dev')
+}
