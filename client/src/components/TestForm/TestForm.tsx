7419a1b4605a149c02041debf39954ffc8e00f54
AnAtoliyAK <aliaksandrau89@gmail.com>
2021-01-15 15:48:17 +0300

    feat: add react-beutyful-dnd

--- client/src/components/TestForm/TestForm.tsx
+++ client/src/components/TestForm/TestForm.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { observer } from 'mobx-react-lite';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { useStore } from '../../hooks/hooks';
 import './TestForm.scss';
 import Task from '../Task/Task';
@@ -7,23 +8,53 @@ import Task from '../Task/Task';
 const TestForm: React.FC = observer(() => {
   const tasksStore = useStore('tasksStore')
 
+  const [tasks, updateTasks] = useState();
+  const handleOnDragEnd = (result: any) => {
+    // if (!result.destination) return;
+    // const items = Array.from(tasks);
+    // const [reorderedItem] = items.splice(result.source.index, 1);
+    // items.splice(result.destination.index, 0, reorderedItem);
+
+    // updateTasks(items);
+  }
+
   return (
     <div className="TestForm">
-      <div>Inbox
-      {(tasksStore.tasks.length) && tasksStore.tasks.map(el => {
-        return (<div key={el._id}>
-          <Task 
-          name={el.list[0].name}
-          priority={el.list[0].priority}
-          taskId={el._id}
-          />
-        </div>)
-      })}
-      </div>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId='inbox'>
+          {(provided) => (<ul
+            {...provided.droppableProps}
+            ref={provided.innerRef}>Inbox
+            {(tasksStore.tasks.length) && tasksStore.tasks.map((el, index) => {
+              return (
+                <Draggable key={el._id} draggableId={el._id} index={index}>
+                  {(provided) => (
+                    <li
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}>
+
+                      <Task
+                        name={el.list[0].name}
+                        priority={el.list[0].priority}
+                        taskId={el._id}
+                      />
+                    </li>
+                  )}
+                </Draggable>
+              )
+            })}
+            {provided.placeholder}
+          </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
+
       <div>SOME_DATE</div>
       <div>ANOTHER_DATE</div>
       <div>SOME_OTHER_DATE</div>
     </div>
+
   )
 });
 
